b598ae0bac1db38e905f705be760122af5c932dd
jacksonPAO <jackson970406@gmail.com>
2019-01-26 00:44:49 -0500

    DevOps rpao udate

--- web/app.py
+++ web/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def hello_world():
-    return 'This is Yoda & Rpao.'
+    return 'This is Yoda & Rpao. update'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000)
