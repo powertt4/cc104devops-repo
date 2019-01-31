9822ccad804b79529f2910eb8462bc602fa797e2
powertt4 <a0933841152@gmail.com>
2019-01-31 20:41:50 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -ex
 USERNAME=204065533127.dkr.ecr.ap-northeast-1.amazonaws.com
 # image name
 #IMAGE=iii-devops-ecr
-IMAGE=rpao_webtest
+IMAGE=cc104devops-repo
 docker build -t $USERNAME/$IMAGE:latest .
 version=`cat VERSION`
 echo "version: $version"
